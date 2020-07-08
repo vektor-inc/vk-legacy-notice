e3f1561f73b3234682279576a7937f5ff31c43b7
rick-addison <rickaddison7634@gmail.com>
2020-07-08 14:41:31 +0900

    少々変更

--- inc/class-vk-legacy-notice-setting.php
+++ inc/class-vk-legacy-notice-setting.php
@@ -33,43 +33,76 @@ class VK_Legacy_Notice_Setting {
 	 * Setting Page
 	 */
 	public static function setting_page() {
-		if ( ! isset( $_GET['check'] ) ) {
+
+		if ( isset( $_GET['check'] ) && 'result' === $_GET['check'] ) {
+
+			$perfect = false;
+
+			$post_templates = array(
+				array(
+					'template'    => 'page-onecolumn.php',
+					'alternative' => __( 'Change Lightning Design Setting > Layout Setting to 1 column or 1 column ( no subsection ).' ),
+				),
+				array(
+					'template'    => 'page-lp-builder.php',
+					'alternative' => __( 'Change Lightning Design Setting > Layout Setting to 1 column ( no subsection ) and fill checkbox that Page Header and Breadcrumb Don\'t display.' ),
+				),
+				array(
+					'template'    => 'page-lp.php',
+					'alternative' => __( 'Change Lightning Design Setting > Layout Setting to 1 column ( no subsection ) and fill checkbox that Page Header and Breadcrumb Don\'t display.' ),
+				),
+				array(
+					'template'    => 'single.php',
+					'alternative' => __( 'Delete single.php or Rename single.php as single-{ post type }.php.' ),
+				),
+				array(
+					'template'    => 'page.php',
+					'alternative' => __( 'Delete page.php.' ),
+				),
+			);
+
+			foreach ( $post_templates as $post_template ) {
+
+				$args = array(
+					'post_type'  => 'any',
+					'meta_key'   => '_wp_page_template',
+					'meta_value' => $post_template['template'],
+				);
+
+				$legacy_posts = get_posts( $args );
+
+				if ( ! empty( $legacy_posts ) ) {
+
+					echo '<p class="legacy-notice">' . esc_html__( 'Legacy template (' . $post_template['template'] . ') is using', 'vk-legacy-notice' ) . '</p>';
+					echo '<p class="legacy-notice">' . esc_html( $post_template['alternative'] ) . '</p>';
+					echo '<ul>';
+					foreach ( $legacy_posts as $legacy_post ) {
+						$legacy_post_list  = '<li>';
+						$legacy_post_list .= get_post_type_object( $legacy_post->post_type )->labels->singular_name;
+						$legacy_post_list .= ' <a href="' . esc_url( get_edit_post_link( $legacy_post->ID ) ) . '">';
+						$legacy_post_list .= esc_html( $legacy_post->post_title );
+						$legacy_post_list .= '</a> ';
+						$legacy_post_list .= esc_html__( 'is used legacy template (' . $post_template['template'] . ').', 'vk-legacy-notice' );
+						$legacy_post_list .= '</li>';
+						echo wp_kses_post( $legacy_post_list );
+					}
+					echo '</ul>';
+					$perfect = false;
+				} else {
+					$perfect = true;
+				}
+			}
+		} else {
 			$explain_text  = '<p class="vk-legacy-notice-paragraph">' . __( 'This plugin can chack old Setting after this sentense', 'vk-legacy-notice' ) . '</p>';
 			$explain_text .= '<ul class="vk-legacy-notice-list">';
 			$explain_text .= '<li>' . __( 'You use legacy functions, isn\'t it?', 'vk-legacy-notice' ) . '</li>';
 			$explain_text .= '<li>' . __( 'You use legacy files, isn\'t it?', 'vk-legacy-notice' ) . '</li>';
 			$explain_text .= '<li>' . __( 'You use legacy options, isn\'t it?', 'vk-legacy-notice' ) . '</li>';
 			$explain_text .= '</ul>';
-			$explain_text .= '<a href="'.admin_url().'?page=vk-legacy-notice-setting&check=result" class="button button-primary">' . __( 'Check legacies now!', 'vk-legacy-notice' ) . '</a>';
+			$explain_text .= '<a href="' . admin_url() . '?page=vk-legacy-notice-setting&check=result" class="button button-primary">' . __( 'Check legacies now!', 'vk-legacy-notice' ) . '</a>';
 
 			echo wp_kses_post( $explain_text );
-		} else if (  isset( $_GET['check'] ) && $_GET['check'] === "result" ) {
-			$args = array(
-				'post_type'  => 'any',
-				'meta_key'   => '_wp_page_template',
-				'meta_value' => 'page-onecolumn.php',
-			);
-
-			$legacy_posts = get_posts( $args );
-
-			if ( ! empty( $legacy_posts ) ) {
-
-				echo '<p class="legacy-notice">' . esc_html__( 'Legacy template \'page-onecolumn.php\' is using', 'vk-legacy-notice' );
-				echo '<ul>';
-				foreach ( $legacy_posts as $legacy_post ) {
-					$legacy_post_list  = '<li>';
-					$legacy_post_list .= get_post_type_object( $legacy_post->post_type )->labels->singular_name;
-					$legacy_post_list .= ' <a href="' . esc_url( get_edit_post_link( $legacy_post->ID ) ) . '">';
-					$legacy_post_list .= esc_html( $legacy_post->post_title );
-					$legacy_post_list .= '</a> ';
-					$legacy_post_list .= esc_html__( 'is used legacy template ( page-onecolumn.php ).', 'vk-legacy-notice' );
-					$legacy_post_list .= '</li>';
-					echo wp_kses_post( $legacy_post_list );
-				}
-				echo '</ul>';
-			}
 		}
-
 	}
 
 }
