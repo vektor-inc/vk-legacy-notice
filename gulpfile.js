8449a8ec729dcf317db85725ce6d6677b4a80f12
kurudrive <kurudrive@gmail.com>
2024-04-10 20:10:09 +0900

    テキストドメインの置換処理削除

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 const gulp = require('gulp')
-const replace = require('gulp-replace')
 
 gulp.task('dist', function (done) {
 	const files = gulp.src(
@@ -17,12 +16,3 @@ gulp.task('dist', function (done) {
 	files.pipe(gulp.dest("dist/vk-legacy-notice"));
 	done();
   });
-
-  // replace_text_domain ////////////////////////////////////////////////
-gulp.task("replace_text_domain", function(done) {
-	// vk-admin
-	gulp.src(["./inc/vk-admin/package/*"])
-		.pipe(replace("vk_admin_textdomain","vk-legacy-notice"))
-		.pipe(gulp.dest("./inc/vk-admin/package/"));
-	done();
-});
